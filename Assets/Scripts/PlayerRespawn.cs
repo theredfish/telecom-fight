dbd6acbdcf2c660b37d02f0a633b005d3dd2de90
icyks <kevin.keovilay@gmail.com>
2018-01-27 00:06:47 +0100

    respawn

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour {
+    public Transform[] spawnPoints;
+    int randomSpawnPoint;
+
+    // Update is called once per frame
+    void SpawnAPlayer(GameObject player)
+    {
+        randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+        player.transform.position = spawnPoints[randomSpawnPoint].position;
+    }
+}
+
