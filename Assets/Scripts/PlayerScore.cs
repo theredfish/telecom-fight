100a7f4a8cd17afbca3c0dd62446d021695b0c59
icyks <kevin.keovilay@gmail.com>
2018-01-27 00:23:27 +0100

    player score script

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour {
+    public int score = 0;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+
+    public void getOneKill()
+    {
+        score += 1;
+    }
+
+    public int getScore()
+    {
+        return this.score;
+    }
+}
