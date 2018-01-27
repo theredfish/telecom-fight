950b80d5eaa171ad32f8a571aa9cc5aa4e7307cc
theredfish <did.julian@gmail.com>
2018-01-27 15:22:08 +0100

    Clean player controller script and release scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,106 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
-    private bool isAlive = true;
-
-    private Rigidbody2D rb2d;
-
+	private Rigidbody2D rb2d;
     private Animator animator;
     private BoxCollider2D sword;
-
     private GameObject[] spawnPoints;
+	private bool isAlive = true;
+	private bool facingRight = true;
+	private bool grounded = false;
+	private float groundRadius = 0.2f;
 
-    public float maxSpeed = 10f;
-    public float jumpForce = 700f;
-    bool facingRight = true;
-    bool grounded = false;
-    public Transform groudCheck;
-    float groundRadius = 0.2f;
-    public LayerMask whatIsGround;
-
-	[Header("Player ID (tmp must be set by the gameManager")]
+	[Header("Player ID (tmp must be set by the gameManager)")]
 	public int id;
 
+	[Header("The maximum speed movement")]
+    public float maxSpeed = 10f;
 
+	[Header("The jump force")]
+    public float jumpForce = 700f;
+
+	[Header("The gameobject to check if the player is grounded")]
+    public Transform groudCheck;
+    
+	[Header("The gameobject on what the player is grounded")]
+    public LayerMask whatIsGround;
 
     // Use this for initialization
     void Start () {
+		this.rb2d = this.gameObject.GetComponent<Rigidbody2D>();
         this.animator = this.gameObject.GetComponent<Animator>();
         this.sword = this.GetComponentsInChildren<BoxCollider2D>()[1];
-        if (!this.sword.tag.Equals("sword"))
-            Debug.Log("Be carreful GetComponentsInChildren NOT working");
+
+		if (!this.sword.tag.Equals ("sword")) {
+			Debug.Log ("Be carreful GetComponentsInChildren NOT working");
+		}
 
         this.spawnPoints = GameObject.FindGameObjectsWithTag("respawnPoint");
-        this.rb2d = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
+    private void Update() {
+		Vector2 jump = new Vector2 (0, jumpForce);
+
         // Jump
-        if (grounded && Input.GetButtonDown("JumpP" + id) && isAlive)
+		if (Input.GetButtonDown("JumpP" + id) && grounded && isAlive)
         {
-            this.rb2d.AddForce(new Vector2(0, jumpForce));
+			this.rb2d.AddForce(jump);
         }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         grounded = Physics2D.OverlapCircle(this.groudCheck.position, this.groundRadius, this.whatIsGround);
 
-        if (isAlive)
-        {
+        if (isAlive) {
             float move = Input.GetAxis("HorizontalP" + id);
             this.rb2d.velocity = new Vector2(move * maxSpeed, this.rb2d.velocity.y);
 
-            if (move > 0 && !facingRight)
-            {
+            if (move > 0 && !facingRight) {
                 Flip();
-            }
-            else if (move < 0 && facingRight)
-            {
+            } else if (move < 0 && facingRight) {
                 Flip();
             }
 
-            if (Input.GetButtonDown("AttackP" + id))
-            {
+            if (Input.GetButtonDown("AttackP" + id)) {
                 this.animator.SetTrigger("attack");
             }
         }
     }
 
-    void Flip()
-    {
+    void Flip() {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
     }
 
-    void Attack()
-    {
+    void Attack() {
         this.sword.enabled = true;
     }
 
-    void StopAttack()
-    {
+    void StopAttack() {
         this.sword.enabled = false;
         this.animator.ResetTrigger("attack");
     }
 
-    void DesactivateControl()
-    {
+    void DesactivateControl() {
        this.isAlive = false;
     }
 
-    void ActivateControl()
-    {
+    void ActivateControl() {
         this.isAlive = true;
     }
 
-    void Respawn()
-    {
+    void Respawn() {
         int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
         this.gameObject.transform.position = spawnPoints[randomSpawnPoint].transform.position;
         this.animator.ResetTrigger("dead");
